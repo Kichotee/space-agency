e823b25a0d7a996f985c9d4d5be58166117cfa52
Kichotee <pt.timone@gmail.com>
2022-12-02 00:35:27 +0100

    img positioning pg3 fix

--- src/views/Crew.vue
+++ src/views/Crew.vue
@@ -17,7 +17,7 @@ lg:bg-[url('../assets/assets/crew/background-crew-desktop.jpg')]  ">
         
             
             <img  v-for="(item, index) in crew"  :key='index' v-show="currentData==index"  class="xs:max-w-[177px] xs:justify-self- mt-8
-              md:absolute bottom-0 lg:right-[50%] lg:translate-x-[50%] md:max-w-[306px] lg:max-w-[326px]   lg:pt-[72px] lg:translate-y-8 " :src='`${crew[index].images.webp}`' alt="">
+              md:absolute md:right-[50%] bottom-0 lg:right-[50%] md:translate-x-[50%] md:max-w-[306px] lg:max-w-[326px]   lg:pt-[72px] lg:translate-y-8 " :src='`${crew[index].images.webp}`' alt="">
            
         </transition-group>
          <hr class=" w-screen md:hidden">
