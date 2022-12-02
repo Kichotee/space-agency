018ab0ab28402264bc8fb31e80ceca26d4865711
Kichotee <pt.timone@gmail.com>
2022-12-02 11:55:08 +0100

    Homepage background fix

--- src/views/Technology.vue
+++ src/views/Technology.vue
@@ -1,9 +1,9 @@
 <template>
-<main class="max-w-[100vw]  landscape:overflow-scroll   h-screen overflow-hidden bg-[url('../assets/assets/technology/background-technology-mobile.jpg')] 
+<main class="max-w-[100vw] h-screen overflow-hidden bg-[url('../assets/assets/technology/background-technology-mobile.jpg')] 
 bg-center bg-no-repeat bg-local bg-cover h-screen text-white 
 md:bg-[url('../assets/assets/technology/background-technology-tablet.jpg')] 
 md:bg-center md:bg-no-repeat md:bg-local md:bg-cover md:h-screen 
-lg:bg-[url('../assets/assets/technology/background-technology-desktop.jpg')]  ">
+lg:bg-[url('../assets/assets/technology/background-technology-desktop.jpg')]  potrait:bg-[url('../assets/assets/technology/background-technology-tablet.jpg')] ">
 
 <techComponent/>
 </main>
